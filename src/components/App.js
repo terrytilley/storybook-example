fa7740ae52ebdc086690b17a4d611484603aa40d
Terry Tilley <terry@terrytilley.com>
2018-03-22 10:45:46 +0000

    added storybook.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const App = () => (
+const App = ({ title }) => (
   <div className="App">
     <header className="App-header">
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Welcome to {title}</h1>
     </header>
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   </div>
 );
+
+App.defaultProps = {
+  title: 'React',
+};
 
 export default App;
